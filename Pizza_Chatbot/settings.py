56938b5399c475261a3b2cd660a56e63899e352d
Toship <sonkusare14@gmail.com>
2020-02-22 20:49:48 +0530

    Changed settings.py for deployment.

--- Pizza_Chatbot/settings.py
+++ Pizza_Chatbot/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0psglb7)gw)@^d*+xp7n2%)y4y_87b1y%*493hv(tpc+_6al)g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
