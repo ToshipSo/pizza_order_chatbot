997ae17fb0fac5f246918c26decb9317fe0e0a40
Toship <sonkusare14@gmail.com>
2020-02-22 21:16:51 +0530

    Debug

--- Pizza_Chatbot/settings.py
+++ Pizza_Chatbot/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0psglb7)gw)@^d*+xp7n2%)y4y_87b1y%*493hv(tpc+_6al)g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
